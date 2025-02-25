0270e5c930b3fcd4a2e7b903ddca6ae74a1fda74
cynumini <cynumini@proton.me>
2025-02-25 18:02:15 +0200

    add usage section to README.md

--- README.md
+++ README.md
@@ -4,56 +4,140 @@
 
 Install [notes](https://github.com/cynumini/notes) and make sure it is available in $PATH.
 
-Using lazy.nvim
+Using lazy.nvim:
 
 ```lua
 return {
-	"cynumini/notes.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	opts = {
-		path = "~/notes"
-	},
+  "cynumini/notes.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    path = "~/notes"
+  },
 }
 ```
 
-You also need to install [telescope.nvim](https://github.com/nvim-telescope/telescope.nvim) for `:NotesSearch'.
+You also need to install [telescope.nvim](https://github.com/nvim-telescope/telescope.nvim) for `:NotesSearch`.
 
-Example setup for [blink.cmp](https://github.com/Saghen/blink.cmp).
+Example setup for [blink.cmp](https://github.com/Saghen/blink.cmp):
 
 ```lua
 return {
-	'saghen/blink.cmp',
-	dependencies = 'rafamadriz/friendly-snippets',
-	version = '*',
-	opts = {
-		keymap = { preset = 'default' },
-		sources = {
-			default = {
-				"lsp", "path", "snippets", "buffer", "omni", "notes"
-			},
-			providers = {
-				notes = {
-					name = 'notes',
-					module = 'notes.blink',
-				},
-			},
-		},
-	}
+  'saghen/blink.cmp',
+  dependencies = 'rafamadriz/friendly-snippets',
+  version = '*',
+  opts = {
+    keymap = { preset = 'default' },
+    sources = {
+      default = {
+        "lsp", "path", "snippets", "buffer", "omni", "notes"
+      },
+      providers = {
+        notes = {
+          name = 'notes',
+          module = 'notes.blink',
+        },
+      },
+    },
+  }
 }
 ```
 
-Next, add this shortcut.
+Next, add this shortcut:
 
 ```lua
 vim.keymap.set("n", "<leader>no", ":NotesSearch<CR>")
 ```
 
-And add these shortcuts to `~/.config/nvim/after/ftplugin/markdown.lua`.
+And add these shortcuts to `~/.config/nvim/after/ftplugin/markdown.lua`:
 
 ```lua
 vim.keymap.set("n", "<leader>nd", ":NotesDo<CR>", { buffer = 0 })
 vim.keymap.set("v", "<leader>ns", ":NotesSortTasks<CR>", { buffer = 0 })
 vim.keymap.set("n", "<CR>", ":NotesOpenLink<CR>", { buffer = 0 })
 ```
+
+## Usage
+
+After setup you have 4 commands and autocompletion for markdown files.
+
+When you create a new file with notes.nvim, use this pattern: `%Y%m%d%H%M%S-title.md`. Inside this file it inserts YAML frontmatter to make sure a NotesSearch command will show it as `title`.
+
+### NotesSearch
+
+This command will search every markdown file in your opts.path and also parse links inside those files and display them as well. To determine the title of the notes, it uses the YAML frontmatter field title or filename.
+
+After you select notes, it opens or creates the notes you selected.
+
+If you want to crate a note based on your input, not on the match, press `<C-y>` in the search prompt.
+
+### NotesOpenLink
+
+If you have wikilinks in your file, and you call `NotesOpenLink` while the cursor is on the link, it will try to find an existing link with the same title and open it, if there is no link with that title, it will create it instead.
+
+Example of a link:
+
+```markdown
+This text with link: [[My other note]].
+```
+
+### NotesDo
+
+If you task on the line with markdown task `- [ ] my task` it mark it done or vice versa.
+
+If the task under the cursor have scheduled like this `- [ ] my task scheduled:<2025-03-22 09:08 .+4w>`, it instead of marking it done, it just reschedule it based on recurrence type. In this case the recurrence is `.+4w`. Besides this program supports 3 types of recurrence and 2 types of recurrence unit (days and weeks). To learn more about how they work, I recommend reading this page of the org-mode documentation [8.3.2 Repeated tasks](https://orgmode.org/manual/Repeated-tasks.html). The only difference is that I've only added support for `SCHEDULED` and there is no warning period.
+
+Although there is also the time, it's not used in NotesDo, but only in NotesSortTasks.
+
+Some other examples of tasks with `scheduled`:
+
+- It means that the task will be repeated every 2 days, and if you miss the day, after completion the new `scheduled` will be 1 day after the current day.
+
+```markdown
+- [ ] Clean my room scheduled:<2025-02-21 08:00 .+2d>
+```
+
+- It mean that task will be repeated every 2 days, and if you miss the day, after completion it will add 2 day to previous `scheduled` until new `scheduled` is greater that currend date.
+
+```markdown
+- [ ] Clean my room scheduled:<2025-02-22 08:00 ++2d>
+```
+
+- It means that the task will be repeated every 2 days, and it's just adding 2 days to the task's `scheduled` date, no matter what.
+
+```markdown
+- [ ] Clean my room scheduled:<2025-02-27 08:00 ++2d>
+```
+
+- There is also support for weekly recurrence.
+
+```markdown
+- [ ] Buy apples scheduled:<2025-02-20 08:00 ++1w>
+```
+
+### NotesSortTasks
+
+This command helps to sort tasks based on the `scheduled` of the task. It uses both time and date in `scheduled`.
+
+Select lines with `V` and then run `:NotesSortTasks`.
+
+Before:
+
+```markdown
+- [ ] Clean my room scheduled:<2025-02-26 08:00 .+1d>
+- [ ] Do Anki scheduled:<2025-02-24 08:10 .+1d>
+- [ ] Buy apples scheduled:<2025-02-24 08:05 .+1d>
+```
+
+After:
+
+```markdown
+- [ ] Buy apples scheduled:<2025-02-24 08:05 .+1d>
+- [ ] Do Anki scheduled:<2025-02-24 08:10 .+1d>
+- [ ] Clean my room scheduled:<2025-02-26 08:00 .+1d>
+```
+
+### Autocompletion
+
+notes.nvim use wikilinks, so to trigger autocompletion type `[[`, then each next symbol will try to find note with matching title.
--- lua/notes/init.lua
+++ lua/notes/init.lua
@@ -18,277 +18,277 @@ local action_state = require "telescope.actions.state"
 ---Initialize notes
 ---@param opts Opts
 M.setup = function(opts)
-  if opts.path == nil then
-    vim.notify("You have to set the path in the notes.nvim setup!", vim.log.levels.ERROR)
-    return
-  end
-  M.path = vim.fn.expand(opts.path)
-  vim.api.nvim_create_user_command("NotesSearch", M.search, {})
-  vim.api.nvim_create_user_command("NotesOpenLink", M.open_link, {})
-  vim.api.nvim_create_user_command("NotesDo", M.do_task, {})
-  vim.api.nvim_create_user_command("NotesSortTasks", M.sort_task, { range = "%" })
+	if opts.path == nil then
+		vim.notify("You have to set the path in the notes.nvim setup!", vim.log.levels.ERROR)
+		return
+	end
+	M.path = vim.fn.expand(opts.path)
+	vim.api.nvim_create_user_command("NotesSearch", M.search, {})
+	vim.api.nvim_create_user_command("NotesOpenLink", M.open_link, {})
+	vim.api.nvim_create_user_command("NotesDo", M.do_task, {})
+	vim.api.nvim_create_user_command("NotesSortTasks", M.sort_task, { range = "%" })
 end
 
 ---Update list of notes
 ---@param sync boolean?
 ---@param on_finish function?
 M.update = function(sync, on_finish)
-  sync = sync or true
-  ---@diagnostic disable-next-line
-  local job = Job:new({
-    command = 'notes',
-    args = { "json", M.path },
-    on_stderr = function(_, result, _)
-      vim.notify(result, vim.log.levels.ERROR)
-    end,
-    on_stdout = function(_, result, _)
-      M.notes = vim.json.decode(result)
-      if on_finish ~= nil then
-        on_finish(M.notes)
-      end
-    end
-  })
-  if sync then
-    job:sync()
-  else
-    job:start()
-  end
+	sync = sync or true
+	---@diagnostic disable-next-line
+	local job = Job:new({
+		command = 'notes',
+		args = { "json", M.path },
+		on_stderr = function(_, result, _)
+			vim.notify(result, vim.log.levels.ERROR)
+		end,
+		on_stdout = function(_, result, _)
+			M.notes = vim.json.decode(result)
+			if on_finish ~= nil then
+				on_finish(M.notes)
+			end
+		end
+	})
+	if sync then
+		job:sync()
+	else
+		job:start()
+	end
 end
 
 local function calc_date(date, value, unit)
-  if unit == "d" then
-    unit = "days"
-  elseif unit == "w" then
-    unit = "weeks"
-  end
-  local result = ""
-  ---@diagnostic disable-next-line
-  Job:new({
-    command = 'date',
-    args = { "-d", date .. " " .. value .. " " .. unit, "--iso-860" },
-    on_stdout = function(_, output, _)
-      result = output:sub(1, #output)
-    end
-  }):sync()
-  return result
+	if unit == "d" then
+		unit = "days"
+	elseif unit == "w" then
+		unit = "weeks"
+	end
+	local result = ""
+	---@diagnostic disable-next-line
+	Job:new({
+		command = 'date',
+		args = { "-d", date .. " " .. value .. " " .. unit, "--iso-860" },
+		on_stdout = function(_, output, _)
+			result = output:sub(1, #output)
+		end
+	}):sync()
+	return result
 end
 
 local function open_note(entry)
-  local exist = entry[2] ~= vim.NIL
-  if not exist then
-    local name = os.date("%Y%m%d%H%M%S-") .. entry[1]:lower():gsub("'", "")
-    local special_chars = { "<", ">", ":", "\"", "/", "\\", "|", "?", "*", " " }
-    for _, value in ipairs(special_chars) do
-      name = name:gsub(value, "_")
-    end
-    ---@diagnostic disable-next-line
-    entry[2] = Path.new(M.path, name).filename .. ".md"
-  end
-  vim.cmd.edit(entry[2])
-  if not exist then
-    local metadata = "---\ntitle: " .. entry[1] .. "\n---\n"
-    vim.api.nvim_paste(metadata, false, -1)
-  end
+	local exist = entry[2] ~= vim.NIL
+	if not exist then
+		local name = os.date("%Y%m%d%H%M%S-") .. entry[1]:lower():gsub("'", "")
+		local special_chars = { "<", ">", ":", "\"", "/", "\\", "|", "?", "*", " " }
+		for _, value in ipairs(special_chars) do
+			name = name:gsub(value, "_")
+		end
+		---@diagnostic disable-next-line
+		entry[2] = Path.new(M.path, name).filename .. ".md"
+	end
+	vim.cmd.edit(entry[2])
+	if not exist then
+		local metadata = "---\ntitle: " .. entry[1] .. "\n---\n"
+		vim.api.nvim_paste(metadata, false, -1)
+	end
 end
 
 ---@param line string
 ---@return table<string, string?>
 local function get_schedule(line)
-  local date, time, recurrence
-  if not date then
-    date, time, recurrence = line:match("scheduled:<(%d%d%d%d%-%d%d%-%d%d) (%d%d:%d%d) ([^>]+)")
-  end
-  if not date then
-    date, time = line:match("scheduled:<(%d%d%d%d%-%d%d%-%d%d) (%d%d:%d%d)>")
-  end
-  if not date then
-    date, recurrence = line:match("scheduled:<(%d%d%d%d%-%d%d%-%d%d) ([^>]+)")
-  end
-  if not date then
-    date = line:match("scheduled:<(%d%d%d%d%-%d%d%-%d%d)>")
-  end
-  return {
-    date = date,
-    time = time,
-    recurrence = recurrence
-  }
+	local date, time, recurrence
+	if not date then
+		date, time, recurrence = line:match("scheduled:<(%d%d%d%d%-%d%d%-%d%d) (%d%d:%d%d) ([^>]+)")
+	end
+	if not date then
+		date, time = line:match("scheduled:<(%d%d%d%d%-%d%d%-%d%d) (%d%d:%d%d)>")
+	end
+	if not date then
+		date, recurrence = line:match("scheduled:<(%d%d%d%d%-%d%d%-%d%d) ([^>]+)")
+	end
+	if not date then
+		date = line:match("scheduled:<(%d%d%d%d%-%d%d%-%d%d)>")
+	end
+	return {
+		date = date,
+		time = time,
+		recurrence = recurrence
+	}
 end
 
 M.search = function(opts)
-  opts = opts or {}
+	opts = opts or {}
 
-  M.update()
+	M.update()
 
-  local results = {}
-  for key, value in pairs(M.notes) do
-    table.insert(results, { key, value })
-  end
+	local results = {}
+	for key, value in pairs(M.notes) do
+		table.insert(results, { key, value })
+	end
 
-  local function select(prompt_bufnr, use_current_line)
-    use_current_line = use_current_line or false
-    actions.close(prompt_bufnr)
+	local function select(prompt_bufnr, use_current_line)
+		use_current_line = use_current_line or false
+		actions.close(prompt_bufnr)
 
-    local selected_entry
-    if not use_current_line then
-      selected_entry = action_state.get_selected_entry()
-    end
+		local selected_entry
+		if not use_current_line then
+			selected_entry = action_state.get_selected_entry()
+		end
 
-    local entry
-    if not selected_entry then
-      entry = { action_state.get_current_line(), vim.NIL }
-    else
-      entry = selected_entry.value
-    end
+		local entry
+		if not selected_entry then
+			entry = { action_state.get_current_line(), vim.NIL }
+		else
+			entry = selected_entry.value
+		end
 
-    open_note(entry)
-  end
+		open_note(entry)
+	end
 
-  local function create(prompt_bufnr)
-    select(prompt_bufnr, true)
-  end
+	local function create(prompt_bufnr)
+		select(prompt_bufnr, true)
+	end
 
-  pickers.new(opts, {
-    prompt_title = "Find or create a note...",
-    finder = finders.new_table {
-      results = results,
-      entry_maker = function(entry)
-        return {
-          value = entry,
-          display = (entry[2] == vim.NIL and " " .. entry[1]) or (" " .. entry[1]),
-          ordinal = entry[1],
-        }
-      end
-    },
-    sorter = conf.generic_sorter(opts),
-    previewer = previewers.new_buffer_previewer({
-      define_preview = function(self, entry)
-        entry = entry.value
-        if entry[2] ~= vim.NIL then
-          ---@diagnostic disable-next-line
-          local lines = vim.fn.readfile(Path.new(M.path, entry[2]).filename)
-          vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, lines)
-          vim.api.nvim_set_option_value('filetype', 'markdown', { buf = self.state.bufnr })
-        end
-      end
-    }),
-    attach_mappings = function(_, map)
-      map("i", "<C-y>", create)
-      actions.select_default:replace(select)
-      return true
-    end,
-  }):find()
+	pickers.new(opts, {
+		prompt_title = "Find or create a note...",
+		finder = finders.new_table {
+			results = results,
+			entry_maker = function(entry)
+				return {
+					value = entry,
+					display = (entry[2] == vim.NIL and " " .. entry[1]) or (" " .. entry[1]),
+					ordinal = entry[1],
+				}
+			end
+		},
+		sorter = conf.generic_sorter(opts),
+		previewer = previewers.new_buffer_previewer({
+			define_preview = function(self, entry)
+				entry = entry.value
+				if entry[2] ~= vim.NIL then
+					---@diagnostic disable-next-line
+					local lines = vim.fn.readfile(Path.new(M.path, entry[2]).filename)
+					vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, lines)
+					vim.api.nvim_set_option_value('filetype', 'markdown', { buf = self.state.bufnr })
+				end
+			end
+		}),
+		attach_mappings = function(_, map)
+			map("i", "<C-y>", create)
+			actions.select_default:replace(select)
+			return true
+		end,
+	}):find()
 end
 
 M.open_link = function()
-  local line = vim.api.nvim_get_current_line()
-  local row = vim.api.nvim_win_get_cursor(0)[2] + 1
+	local line = vim.api.nvim_get_current_line()
+	local row = vim.api.nvim_win_get_cursor(0)[2] + 1
 
-  local reverse_line = line:sub(1, row):reverse()
-  local reverse = reverse_line:find("[[", 1, true)
+	local reverse_line = line:sub(1, row):reverse()
+	local reverse = reverse_line:find("[[", 1, true)
 
-  -- If there is no [[ in front of the cursor, it means there is no link.
-  if not reverse then
-    return
-  end
+	-- If there is no [[ in front of the cursor, it means there is no link.
+	if not reverse then
+		return
+	end
 
-  local start_of_link = #reverse_line - reverse
-  local end_of_link = start_of_link + line:sub(start_of_link, #line):find("]]", 1, true)
+	local start_of_link = #reverse_line - reverse
+	local end_of_link = start_of_link + line:sub(start_of_link, #line):find("]]", 1, true)
 
-  -- If there is no ]] after [[, it means there is no link.
-  if not end_of_link then
-    return
-  end
+	-- If there is no ]] after [[, it means there is no link.
+	if not end_of_link then
+		return
+	end
 
-  local link = line:sub(start_of_link + 2, end_of_link - 2)
-  M.update()
-  for key, value in pairs(M.notes) do
-    if key == link then
-      open_note({ key, value })
-      return
-    end
-  end
-  open_note({ link, vim.NIL })
+	local link = line:sub(start_of_link + 2, end_of_link - 2)
+	M.update()
+	for key, value in pairs(M.notes) do
+		if key == link then
+			open_note({ key, value })
+			return
+		end
+	end
+	open_note({ link, vim.NIL })
 end
 
 M.do_task = function()
-  local line = vim.api.nvim_get_current_line()
+	local line = vim.api.nvim_get_current_line()
 
-  local is_done = false
+	local is_done = false
 
-  -- It's task?
-  if not line:find("- [ ]", 1, true) then
-    if line:find("- [x]", 1, true) then
-      is_done = true
-    else
-      return
-    end
-  end
+	-- It's task?
+	if not line:find("- [ ]", 1, true) then
+		if line:find("- [x]", 1, true) then
+			is_done = true
+		else
+			return
+		end
+	end
 
-  local schedule = get_schedule(line)
+	local schedule = get_schedule(line)
 
-  -- If task does not have recurrence, then just done or undone it
-  if not schedule.recurrence and not is_done then
-    line = line:gsub("- %[ %]", "- [x]")
-  elseif not schedule.recurrence and is_done then
-    line = line:gsub("- %[x%]", "- [ ]")
-  end
-  if schedule.recurrence then
-    -- Unit is always the last character in "recurrence"
-    local unit = schedule.recurrence:sub(#schedule.recurrence, #schedule.recurrence)
-    local current_date = os.date("%Y-%m-%d")
+	-- If task does not have recurrence, then just done or undone it
+	if not schedule.recurrence and not is_done then
+		line = line:gsub("- %[ %]", "- [x]")
+	elseif not schedule.recurrence and is_done then
+		line = line:gsub("- %[x%]", "- [ ]")
+	end
+	if schedule.recurrence then
+		-- Unit is always the last character in "recurrence"
+		local unit = schedule.recurrence:sub(#schedule.recurrence, #schedule.recurrence)
+		local current_date = os.date("%Y-%m-%d")
 
-    -- ++ is when we add the recurrence value until the new date is greater than today.
-    -- .+ is when we ignore the task's scheduled date and just add the recurrence value.
-    -- + is when we add the recurrence value to the scheduled date of the task.
-    if schedule.recurrence:sub(1, 2) == "++" then
-      local value = tonumber(schedule.recurrence:sub(3, #schedule.recurrence - 1))
-      while schedule.date <= current_date do
-        schedule.date = calc_date(schedule.date, value, unit)
-      end
-    elseif schedule.recurrence:sub(1, 2) == ".+" then
-      local value = tonumber(schedule.recurrence:sub(3, #schedule.recurrence - 1))
-      schedule.date = calc_date(current_date, value, unit)
-    elseif schedule.recurrence:sub(1, 1) == "+" then
-      local value = tonumber(schedule.recurrence:sub(2, #schedule.recurrence - 1))
-      schedule.date = calc_date(schedule.date, value, unit)
-    end
+		-- ++ is when we add the recurrence value until the new date is greater than today.
+		-- .+ is when we ignore the task's scheduled date and just add the recurrence value.
+		-- + is when we add the recurrence value to the scheduled date of the task.
+		if schedule.recurrence:sub(1, 2) == "++" then
+			local value = tonumber(schedule.recurrence:sub(3, #schedule.recurrence - 1))
+			while schedule.date <= current_date do
+				schedule.date = calc_date(schedule.date, value, unit)
+			end
+		elseif schedule.recurrence:sub(1, 2) == ".+" then
+			local value = tonumber(schedule.recurrence:sub(3, #schedule.recurrence - 1))
+			schedule.date = calc_date(current_date, value, unit)
+		elseif schedule.recurrence:sub(1, 1) == "+" then
+			local value = tonumber(schedule.recurrence:sub(2, #schedule.recurrence - 1))
+			schedule.date = calc_date(schedule.date, value, unit)
+		end
 
-    -- Replace old schedule with new one, check if there was time and keep it
-    if schedule.time then
-      line = line:gsub("scheduled:<.+>",
-        "scheduled:<" .. schedule.date .. " " .. schedule.time .. " " .. schedule.recurrence .. ">")
-    else
-      line = line:gsub("scheduled:<.+>", "scheduled:<" .. schedule.date .. " " .. schedule.recurrence .. ">")
-    end
-  end
-  vim.api.nvim_set_current_line(line)
+		-- Replace old schedule with new one, check if there was time and keep it
+		if schedule.time then
+			line = line:gsub("scheduled:<.+>",
+				"scheduled:<" .. schedule.date .. " " .. schedule.time .. " " .. schedule.recurrence .. ">")
+		else
+			line = line:gsub("scheduled:<.+>", "scheduled:<" .. schedule.date .. " " .. schedule.recurrence .. ">")
+		end
+	end
+	vim.api.nvim_set_current_line(line)
 end
 
 M.sort_task = function()
-  local current_buffer = vim.api.nvim_get_current_buf()
-  local start_line = vim.api.nvim_buf_get_mark(current_buffer, "<")[1] - 1
-  local end_line = vim.api.nvim_buf_get_mark(current_buffer, ">")[1]
-  local lines = vim.api.nvim_buf_get_lines(current_buffer, start_line, end_line, true)
-  for i = #lines, 1, -1 do
-    if lines[i] == "" then
-      table.remove(lines, i)
-    end
-  end
-  table.sort(lines, function(a, b)
-    local a_scheduled = get_schedule(a)
-    local b_scheduled = get_schedule(b)
-    if a_scheduled.date == nil and b_scheduled.date == nil then
-      return a < b
-    elseif a_scheduled.date == nil and b_scheduled.date ~= nil then
-      return false
-    elseif b_scheduled.date == nil and a_scheduled.date ~= nil then
-      return true
-    end
-    local a_scheduled_string = a_scheduled.date .. (a_scheduled.time or "23:59")
-    local b_scheduled_string = b_scheduled.date .. (b_scheduled.time or "23:59")
-    return a_scheduled_string < b_scheduled_string
-  end)
-  vim.api.nvim_buf_set_lines(current_buffer, start_line, end_line, true, lines)
+	local current_buffer = vim.api.nvim_get_current_buf()
+	local start_line = vim.api.nvim_buf_get_mark(current_buffer, "<")[1] - 1
+	local end_line = vim.api.nvim_buf_get_mark(current_buffer, ">")[1]
+	local lines = vim.api.nvim_buf_get_lines(current_buffer, start_line, end_line, true)
+	for i = #lines, 1, -1 do
+		if lines[i] == "" then
+			table.remove(lines, i)
+		end
+	end
+	table.sort(lines, function(a, b)
+		local a_scheduled = get_schedule(a)
+		local b_scheduled = get_schedule(b)
+		if a_scheduled.date == nil and b_scheduled.date == nil then
+			return a < b
+		elseif a_scheduled.date == nil and b_scheduled.date ~= nil then
+			return false
+		elseif b_scheduled.date == nil and a_scheduled.date ~= nil then
+			return true
+		end
+		local a_scheduled_string = a_scheduled.date .. (a_scheduled.time or "23:59")
+		local b_scheduled_string = b_scheduled.date .. (b_scheduled.time or "23:59")
+		return a_scheduled_string < b_scheduled_string
+	end)
+	vim.api.nvim_buf_set_lines(current_buffer, start_line, end_line, true, lines)
 end
 
 return M
